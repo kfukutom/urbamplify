ee171143bfff330be17aa8891703a0ee8df567ef
kfukutom <kfukutom@umich.edu>
2024-10-29 16:37:10 -0400

    smoother toggle animation

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,17 +1,18 @@
+import React from "react";
 import "./Buttons.css";
 
-export const Buttons = ({ isDark }) => {
+export const Buttons = ({ isDark, handleNavigate }) => {
   return (
     <div className="btn-container">
       <button
         id="check"
         className={`button-19 ${isDark ? "dark-mode" : "light-mode"}`}
+        onClick={handleNavigate}  // Calls the handleNavigate function from props
       >
-        <a href="https://www.linkedin.com/in/narayan-aakash/">
-          Explore
-        </a>
+        Get Started
       </button>
     </div>
   );
 };
 
+export default Buttons;
