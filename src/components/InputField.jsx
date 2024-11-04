5b59b60ad7f8b6ac0a3041dcc2ff1a90578e74b6
kfukutom <kfukutom@umich.edu>
2024-11-04 15:01:29 -0500

    New issue

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,20 +1,28 @@
 import { useState } from "react";
 
-const InputField = ({ type, placeholder, icon }) => {
+const InputField = ({ type, placeholder, icon, onChange }) => {
   const [isPasswordShown, setIsPasswordShown] = useState(false);
+
+  const handleChange = (e) => {
+    onChange(e.target.value);
+  };
 
   return (
     <div className="input-wrapper">
       <input
-        type={isPasswordShown ? 'text' : type}
+        type={isPasswordShown ? "text" : type}
         placeholder={placeholder}
         className="input-field"
         required
+        onChange={handleChange} // Call onChange function when input changes
       />
       <i className="material-symbols-rounded">{icon}</i>
-      {type === 'password' && (
-        <i onClick={() => setIsPasswordShown(prevState => !prevState)} className="material-symbols-rounded eye-icon">
-          {isPasswordShown ? 'visibility' : 'visibility_off'}
+      {type === "password" && (
+        <i
+          onClick={() => setIsPasswordShown((prevState) => !prevState)}
+          className="material-symbols-rounded eye-icon"
+        >
+          {isPasswordShown ? "visibility" : "visibility_off"}
         </i>
       )}
     </div>
